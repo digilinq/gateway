65b1325f4c730fb847774ea13b02a9c173a91f26
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2023-12-10 09:18:22 +0100

    Add security config

---
+++ src/main/java/com/eightbits/infrastructure/gateway/configurations/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.eightbits.infrastructure.gateway.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(requestMatcherRegistry ->
+                        requestMatcherRegistry.requestMatchers("/login", "/signup").permitAll()
+                                .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
