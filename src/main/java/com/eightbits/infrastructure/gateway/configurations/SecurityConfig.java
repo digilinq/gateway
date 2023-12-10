5ab587cbf2fefd07369dd51d50b69911e951d3e6
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2023-12-10 18:24:59 +0100

    Use WebFlux security

--- src/main/java/com/eightbits/infrastructure/gateway/configurations/SecurityConfig.java
+++ src/main/java/com/eightbits/infrastructure/gateway/configurations/SecurityConfig.java
@@ -2,23 +2,17 @@ package com.eightbits.infrastructure.gateway.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(requestMatcherRegistry ->
-                        requestMatcherRegistry.requestMatchers("/login", "/signup").permitAll()
-                                .anyRequest().authenticated())
-                .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login").permitAll())
-                .logout(LogoutConfigurer::permitAll)
-                .csrf(AbstractHttpConfigurer::disable);
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchange -> exchange.anyExchange().permitAll())
+                .csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
